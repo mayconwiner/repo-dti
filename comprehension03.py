31b7c5b658fc219961c017892f15fa3c67441ee7
Maycon <mayconwiner@gmail.com>
2024-08-10 01:09:03 -0300

    comprehension03

---
+++ comprehension03.py
@@ -0,0 +1,7 @@
+vendas_produtos = [1500,150,2100,1950]
+produtos = ['vinho','cerveja','refrigerante','suco']
+
+lista_aux = list(zip(vendas_produtos,produtos))
+lista_aux.sort(reverse=True)
+produtos = [produto for vendas,produto in lista_aux]
+print(produtos)
