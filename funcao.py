4efcfe782861842cd1a0d168ed516c183a702236
Maycon <mayconwiner@gmail.com>
2024-08-10 01:09:02 -0300

    funcao_calculo_meta

---
+++ funcao.py
@@ -0,0 +1,28 @@
+meta = 10000
+vendas = {
+    'João': 15000,
+    'Julia': 27000,
+    'Marcus': 9900,
+    'Maria': 37500,
+    'Ana': 10300,
+    'Alon': 7870
+}
+
+def calculo_meta(meta,vendas):
+    '''Calcula a porcentagem de vendedores que bateram a meta e retorna uma lista com os vendedores que bateram a meta'''
+    bateram_meta = []
+    for vendedor in vendas:
+        if vendas[vendedor] >= meta:
+            bateram_meta.append(vendedor)
+    perc_baterammeta = len(bateram_meta) / len(vendas) * 100
+    '''Retorna a porcentagem de vendedores que bateram a meta e uma lista com os vendedores que bateram a meta'''
+    return perc_baterammeta,bateram_meta
+
+p_meta, vendedores_acima_meta = calculo_meta(meta, vendas)
+print(p_meta)
+print(vendedores_acima_meta)
+
+
+
+
+
